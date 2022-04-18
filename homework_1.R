5c20b1999e2b87dcec196eb596c295cff5d58eb7
Iliyan <ilian.dd00@gmail.com>
2022-04-18 23:10:57 +0300

    Task 2

---
+++ homework_1.R
@@ -0,0 +1,36 @@
+library("UsingR")
+
+
+# Задача 1
+# ...
+
+
+# Задача 2
+# 2.1
+mtcars[1:5, 1]
+
+# 2.2
+rownames(mtcars)[which.max(mtcars$hp)]
+rownames(mtcars[order(mtcars$hp, decreasing = T), ])[1:5]
+
+# 2.3
+# тук не съм сигурен дали това се иска
+boxplot(mtcars$mpg, mtcars$cyl)
+
+# 2.4
+plot(mtcars$hp, mtcars$mpg)
+cor(mtcars$hp, mtcars$mpg)
+
+# 2.5
+quantile(mtcars$hp, 0.80)
+
+# 2.6
+length(mtcars$hp[mtcars$hp < 100]) / length(mtcars$hp) * 100
+
+# 2.7
+t = table(mtcars$cyl, mtcars$gear)
+t["8", "5"] / sum(t[, "5"])
+
+
+# Задача 3
+# ...
